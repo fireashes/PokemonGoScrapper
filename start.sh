e06c77cc2587d47815bef68041ba565ca28982b7
Ashes <Fire3Ashes@gmail.com>
2018-08-21 18:41:52 -0500

    Added log to simpleLogger.log file

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ sortUnique(){
     echo "${1}"
     src/main/sh/sortUniqueProperties.sh "${1}"
 }
-for f in ./src/main/data/PokemonCom/*/*/*.properties ./src/main/data/PokemonCom/*.properties; do
+for f in ./src/main/data/PokemonCom/*/*/*.properties ./src/main/data/PokemonCom/*.txt; do
     sortUnique "${f}"
 done
 
 ./gradlew clean build
 #./gradlew clean test --tests "com.caliyeti.PokemonComTest.testPokemonEvolution"
 
-for f in ./src/main/data/PokemonCom/*/*/*.properties ./src/main/data/PokemonCom/*.properties; do
+for f in ./src/main/data/PokemonCom/*/*/*.properties ./src/main/data/PokemonCom/*.txt; do
     sortUnique "${f}"
 done
 
